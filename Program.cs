411aab90b241b8147b1533168c9895cf9a9c3908
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 09:53:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Draw__shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      /|");
+            Console.WriteLine("     / |");
+            Console.WriteLine("    /  |");
+            Console.WriteLine("   /   |");
+            Console.WriteLine("   ____");
+            Console.WriteLine();
+        }
+        
+    }
+}
